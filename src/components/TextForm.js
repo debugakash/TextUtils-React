ed051740f9ca4de7d7950630dcf351cd1ad96eb1
Akash Arya <118762199+debugakash@users.noreply.github.com>
2022-12-26 19:11:26 +0530

    Buttons colour updated

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,10 +39,10 @@ export default function TextForm(props) {
                 <div className=" mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="6"></textarea>
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={ClearText}>Clear Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-success mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button disabled={text.length === 0} className="btn btn-warning mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button disabled={text.length === 0} className="btn btn-danger mx-1 my-1" onClick={ClearText}>Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-1 my-1" onClick={handleCopy}>Copy Text</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Extra Spaces</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
